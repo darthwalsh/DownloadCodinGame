233ca6cd6da0863150d1412931b4a615f32e1d5d
Carl Walsh <darthwalsh@gmail.com>
2021-07-02 09:47:27 -0700

    add feed script
    Goal is to poll codingame API using serverless function
    Then create RSS feed based on results.

---
+++ feed/index.js
@@ -0,0 +1,42 @@
+const fetch = require("node-fetch");
+
+async function api(endpoint, body) {
+  const method = "POST";
+  console.error(method, endpoint, body);
+  const res = await fetch(`https://www.codingame.com/services/${endpoint}`, {
+    method,
+    headers: {
+      accept: "application/json, text/plain, */*",
+    },
+    body: JSON.stringify(body),
+  });
+
+  console.error(res.status);
+  return res.json();
+}
+
+async function run() {
+  const minimalProgress = await api("Puzzle/findAllMinimalProgress", [null]);
+  console.log("Got", minimalProgress.length, "puzzles");
+
+  minimalProgress.sort((a, b) => b.creationTime - a.creationTime);
+  const top = minimalProgress.slice(0, 10);
+
+  const details = await api("Puzzle/findProgressByIds", [
+    top.map((p) => p.id),
+    null,
+    2,
+  ]);
+
+  console.error(details)
+  for (const p of details) {
+    // Could do more with Topics, Description, Cover banner, Contributor, XP, etc.
+    const { level, title, detailsPageUrl, type, creationTime } = p;
+    const url = `https://www.codingame.com${detailsPageUrl}`;
+    console.error(new Date(creationTime), type, level, title);
+    console.error(url);
+    console.error();
+  }
+}
+
+run();
