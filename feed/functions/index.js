57cc8b1f41acdb41ee7b3dc0507c75e1394d18ba
Carl Walsh <darthwalsh@gmail.com>
2021-07-04 11:45:26 -0700

    refactor index into a reusable module
    Also make a script
    Move debugging to use debug module

--- feed/functions/index.js
+++ feed/functions/index.js
@@ -1,9 +1,13 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const recentPuzzles = require("../index");
+
+exports.scheduledFunction = functions.pubsub.schedule("every 4 hours").onRun(async context => {
+  // TODO does it work?
+  const puzzles = await recentPuzzles();
+  for (const p of puzzles) {
+    const {level, title, url, created} = p;
+    console.log(created, level, title, url);
+  }
+});
+
--- feed/index.js
+++ feed/index.js
@@ -1,8 +1,20 @@
+const debug = require("debug");
 const fetch = require("node-fetch");
+
+const log = debug("downloadcodingame");
+
+/**
+ * @typedef {Object} Puzzle
+ * @property {number} id - immutable ID
+ * @property {string} title - i.e. name of the puzzle
+ * @property {Date} created - when puzzle created TODO UTC?
+ * @property {string} level - i.e. easy/hard
+ * @property {string} url - codingame.com puzzle details
+ */
 
 async function api(endpoint, body) {
   const method = "POST";
-  console.error(method, endpoint, body);
+  log(method, endpoint, body);
   const res = await fetch(`https://www.codingame.com/services/${endpoint}`, {
     method,
     headers: {
@@ -11,31 +23,39 @@ async function api(endpoint, body) {
     body: JSON.stringify(body),
   });
 
-  console.error(res.status);
+  log(res.status);
   return res.json();
 }
 
+/** @returns {Promise<Puzzle[]>} */
 async function run() {
   const minimalProgress = await api("Puzzle/findAllMinimalProgress", [null]);
-  console.log("Got", minimalProgress.length, "puzzles");
 
   const top = minimalProgress.sort((a, b) => b.creationTime - a.creationTime).slice(0, 10);
 
-  const details = await api("Puzzle/findProgressByIds", [
-    top.map((p) => p.id),
-    null,
-    2,
-  ]);
+  const details = await api("Puzzle/findProgressByIds", [top.map(p => p.id), null, 2]);
 
-  console.error(details.sort((a, b) => b.creationTime - a.creationTime))
-  for (const p of details) {
-    // Could do more with Topics, Description, Cover banner, Contributor, XP, etc.
-    const { level, title, detailsPageUrl, type, creationTime } = p;
-    const url = `https://www.codingame.com${detailsPageUrl}`;
-    console.error(new Date(creationTime), type, level, title);
-    console.error(url);
-    console.error();
+  return details
+    .sort((a, b) => b.creationTime - a.creationTime)
+    .map(p => {
+      // Could do more with Topics, Description, Cover banner, Contributor, Type, XP, etc.
+      const {id, level, title, detailsPageUrl, creationTime} = p;
+      const created = new Date(creationTime);
+      const url = `https://www.codingame.com${detailsPageUrl}`;
+      return {id, title, created, level, url};
+    });
+}
+
+async function main() {
+  const puzzles = await run();
+  for (const p of puzzles) {
+    const {level, title, url, created} = p;
+    console.log(created, level, title, url);
   }
 }
 
-run();
+if (require.main === module) {
+  main();
+}
+
+module.exports = run;
