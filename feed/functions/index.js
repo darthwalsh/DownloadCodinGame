49685271fd32982774733ee8f07be5634b73046d
Carl Walsh <darthwalsh@gmail.com>
2021-07-09 15:06:49 -0700

    Generate RSS feed

--- feed/functions/index.js
+++ feed/functions/index.js
@@ -1,13 +1,38 @@
 const functions = require("firebase-functions");
+const Feed = require("feed").Feed;
 
 const recentPuzzles = require("../index");
 
-exports.scheduledFunction = functions.pubsub.schedule("every 4 hours").onRun(async context => {
-  // TODO does it work?
+const refreshHours = 4;
+
+function createFeed() {
+  return new Feed({
+    title: "DownloadCodinGame Latest Puzzles",
+    description: "Recent puzzles from CodinGame https://github.com/darthwalsh/DownloadCodinGame",
+    link: "https://www.codingame.com/training",
+    language: "en",
+    copyright: "See CodinGame ToS",
+    ttl: refreshHours * 60,
+  });
+}
+
+const cron = `every ${refreshHours} hours`;
+exports.scheduledFunction = functions.pubsub.schedule(cron).onRun(async () => {
+  const feed = createFeed();
+
   const puzzles = await recentPuzzles();
   for (const p of puzzles) {
-    const {level, title, url, created} = p;
-    console.log(created, level, title, url);
-  }
-});
+    const {id, level, title, url, created} = p;
+    console.log(id, created, level, title, url);
 
+    feed.addItem({
+      title: `[${level.toLocaleUpperCase()}] ${title}`,
+      id,
+      link: url,
+      date: created,
+    });
+  }
+
+  console.log();
+  console.log(feed.rss2());
+});
