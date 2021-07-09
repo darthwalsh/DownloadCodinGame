df46c03e5a1b6d75b925cdb198155b51f81a59e7
Carl Walsh <darthwalsh@gmail.com>
2021-07-09 15:58:40 -0700

    merge together to a single file that correctly uploads

---
+++ feed/README.md
@@ -0,0 +1,3 @@
+# RSS Feed
+
+Cloud function that polls CodinGame and publishes RSS feed: https://download-codingame.appspot.com.storage.googleapis.com/recent/feed.rss
--- feed/functions/index.js
+++ feed/functions/index.js
@@ -1,15 +1,54 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-
+const fetch = require("node-fetch");
 const Feed = require("feed").Feed;
-
-const recentPuzzles = require("../index");
 
 admin.initializeApp();
 const bucket = admin.storage().bucket();
 
-const refreshHours = 4;
+async function api(endpoint, body) {
+  const method = "POST";
+  console.log(method, endpoint, body);
+  const res = await fetch(`https://www.codingame.com/services/${endpoint}`, {
+    method,
+    headers: {
+      accept: "application/json, text/plain, */*",
+    },
+    body: JSON.stringify(body),
+  });
 
+  console.log(res.status);
+  return res.json();
+}
+
+/**
+ * @typedef {Object} Puzzle
+ * @property {number} id - immutable ID
+ * @property {string} title - i.e. name of the puzzle
+ * @property {Date} created - when puzzle created TODO UTC?
+ * @property {string} level - i.e. easy/hard
+ * @property {string} url - codingame.com puzzle details
+ */
+/** @returns {Promise<Puzzle[]>} */
+async function recentPuzzles() {
+  const minimalProgress = await api("Puzzle/findAllMinimalProgress", [null]);
+
+  const top = minimalProgress.sort((a, b) => b.creationTime - a.creationTime).slice(0, 10);
+
+  const details = await api("Puzzle/findProgressByIds", [top.map(p => p.id), null, 2]);
+
+  return details
+    .sort((a, b) => b.creationTime - a.creationTime)
+    .map(p => {
+      // Could do more with Topics, Description, Cover banner, Contributor, Type, XP, etc.
+      const {id, level, title, detailsPageUrl, creationTime} = p;
+      const created = new Date(creationTime);
+      const url = `https://www.codingame.com${detailsPageUrl}`;
+      return {id, title, created, level, url};
+    });
+}
+
+const refreshHours = 4;
 function createFeed() {
   return new Feed({
     title: "DownloadCodinGame Latest Puzzles",
